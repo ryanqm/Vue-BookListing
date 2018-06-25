6eed498c43fe386150336830c4c31a83501fd708
Ryan Briscall <ryan.briscall@questionmark.com>
2018-06-25 14:55:24 -0600

    setup books array

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
   <h1>{{title}}</h1>
+  <ul>
+    <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+  </ul>
 </div>
 </template>
 
@@ -9,7 +12,12 @@ export default {
   name: 'BookList',
   data() {
     return {
-      title: 'All Books'
+      title: 'All Books',
+      books: [
+        {title: 'Book 1', author: 'Jimmy'},
+        {title: 'Book 2', author: 'John'},
+        {title: 'Book 3', author: 'Sarah'},
+      ]
     }
   }
 }
@@ -19,4 +27,6 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+ul { list-style: none; padding: 0; }
+li { margin: 0 10px; }
 </style>
